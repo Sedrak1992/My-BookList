03269ea0531365e28336041abe825a319fb53a5d
Sedrak Qocharyan <sedrakqocharyan@gmail.com>
2022-06-24 13:15:09 +0400

    aa

--- index.js
+++ index.js
@@ -39,15 +39,6 @@ class UI {
     }
   }
 
-  // static showAlert(message, className) {
-  //   const div = document.createElement("div");
-  //   div.className = `alert alert-${className}`;
-  //   div.appendChild(document.createTextNode(message));
-  //   const container = document.querySelector(".container");
-  //   const form = document.querySelector("#book-form");
-  //   container.insertBefore(div, form);
-  // }
-
   static clearFields() {
     document.querySelector("#title").value = "";
     document.querySelector("#author").value = "";
